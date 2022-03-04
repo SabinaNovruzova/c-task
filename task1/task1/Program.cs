9a73314059b666afcfeeafaa79605b036eb9cfbf
Sabina Novruzova <tu7cldm19@aztu.local>
2022-03-04 14:18:56 +0400

    added tasks

---
+++ task1/task1/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace task1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = Convert.ToInt32(Console.ReadLine());
+            int m = Convert.ToInt32(Console.ReadLine());
+            int count = 0;
+            for (int i = n; i < m; i++)
+            {
+                if (i % 7 == 0 && i!=0)
+                {
+                    count++;
+                }
+            }
+            Console.WriteLine(count);
+        }
+    }
+}
---
+++ task2/task2/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace task2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string Ay;
+            Console.WriteLine("Ay yazin");
+            Ay = Console.ReadLine();
+            switch (Ay)
+            {
+                case "Fevral":
+                    Console.WriteLine(28);
+                    break;
+                case "Aprel":
+                case "Iyun":
+                case "Sentyabr":
+                case "Noyabr":
+                    Console.WriteLine(30);
+                    break;
+                case "Yanvar":
+                case "Mart":
+                case "May":
+                case "Iyul":
+                case "Avqust":
+                case "Oktyabr":
+                case "Dekabr":
+                    Console.WriteLine(31);
+                    break;
+                default:
+                    Console.WriteLine("bele gun yoxdu");
+                    break;
+            }
+        }
+    }
+}
